aca1603714c74270b7cfbc5220a756da326c47b9
raghavpatidar <raghavpatidar997764@gmail.com>
2023-12-19 21:40:55 +0530

    Day-3-SankeLaderSimulator

--- Day1/src/Main.java
+++ Day1/src/Main.java
@@ -1,5 +1,42 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+
+        
+
+
+//        Patter1
+//        Scanner scanner = new Scanner(System.in);
+//        int N = scanner.nextInt();
+//
+//        for (int i = 1 ; i<=N; i++){
+//            for (int j = 1; j <=i ; j++){
+//                System.out.print(j);
+//            }
+//            System.out.println();
+//        }
+
+//        Pattern2
+//        Scanner scanner = new Scanner(System.in);
+//        int N = scanner.nextInt();
+//        for (int i = 0; i <= N ; i++){
+//
+//                if(i ==0 || i == N){
+//                    for (int j =  0 ; j < N ;j++){
+//                        System.out.print("* ");
+//                    }
+//                    System.out.println("");
+//
+//                }else{
+//                    System.out.print("* ");
+//                    for (int j =  1 ; j < N-1 ;j++){
+//                        System.out.print("  ");
+//                    }
+//                    System.out.println("*");
+//                }
+//
+//        }
     }
 }
---
+++ SnakeLadderSimulater/src/SnakeLadderSimulator.java
@@ -0,0 +1,5 @@
+public class SnakeLadderSimulator {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Snake And Ladder Simulater");
+    }
+}
